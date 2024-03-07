37e766a9e76d198628eeffc40dc036dad1802409
AkhilaPodilapu5 <akhila.podilapu@atos.net>
2024-03-07 16:31:25 +0530

    Update generate_payload.sh

--- generate_payload.sh
+++ generate_payload.sh
@@ -3,7 +3,9 @@
 # Perform actions to generate diff, author ID, and reviewer details
 diff_data=$(git diff HEAD^ HEAD)
 author_id=$(git log --format="%an" -n 1)
-reviewer_details=$(git remote get-url origin | sed -n 's#.*/\([^.]*\)\.git#\1#p')
+reviewer_details=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${{ github.event.pull_request.number }}/reviews" \
+  | jq -r '.[0].user.login')
 pull_request_url=$(git rev-parse --show-toplevel)/.github/pull_request_url
 # Create payload JSON
 payload='{
