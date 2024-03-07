6c463c65081f1ce50140da49ecd8174441483744
AkhilaPodilapu5 <akhila.podilapu@atos.net>
2024-03-07 16:21:48 +0530

    Update generate_payload.sh

--- generate_payload.sh
+++ generate_payload.sh
@@ -3,8 +3,7 @@
 # Perform actions to generate diff, author ID, and reviewer details
 diff_data=$(git diff HEAD^ HEAD)
 author_id=$(git log --format="%an" -n 1)
-reviewer_details=${{ github.repository_owner }}
-
+reviewer_details=$(git remote get-url origin | sed -n 's#.*/\([^.]*\)\.git#\1#p')
 # Create payload JSON
 payload='{
   "diff": "'"$diff_data"'",
