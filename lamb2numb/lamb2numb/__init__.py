7938234a31b48c6ba32ad94ce28bee94daef08ea
AkhilaPodilapu5 <akhila.podilapu@atos.net>
2024-03-08 17:08:14 +0530

    Update __init__.py

--- lamb2numb/lamb2numb/__init__.py
+++ lamb2numb/lamb2numb/__init__.py
@@ -1,2 +1,3 @@
 """Lambda to load numpy array from S3 bucket on AWS Event."""
 He;lloWorld!!!!!!!!!!!!!!!!!content written to lamb2numb in init.py
+Adding one more line after adding test file to patch-1 branch and test whether pr is fetching this or not
